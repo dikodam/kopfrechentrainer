cd6d5dbc6d56dce7392df4ffde66c4c0b6a49a04
adiko <dikodam@googlemail.com>
2017-07-14 21:00:46 +0200

    wtf

--- src/main/java/de/dikodam/libs/kopfrechentrainer/KopfrechenTrainer.java
+++ src/main/java/de/dikodam/libs/kopfrechentrainer/KopfrechenTrainer.java
@@ -2,4 +2,91 @@ package de.dikodam.libs.kopfrechentrainer;
 
 public class KopfrechenTrainer {
 
+    private int minStellenanzahl1;
+    private int minStellenanzahl2;
+    private int maxStellenanzahl1;
+    private int maxStellenanzahl2;
+
+    private boolean plusErlaubt;
+    private boolean minusErlaubt;
+    private boolean malErlaubt;
+    private boolean geteiltErlaubt;
+
+
+    public KopfrechenTrainer() {
+        minStellenanzahl1 = 1;
+        minStellenanzahl2 = 1;
+        maxStellenanzahl1 = 2;
+        maxStellenanzahl2 = 2;
+    }
+
+    public int getMinStellenanzahl1() {
+        return minStellenanzahl1;
+    }
+
+    public void setMinStellenanzahl1(int minStellenanzahl1) {
+        this.minStellenanzahl1 = minStellenanzahl1;
+    }
+
+    public int getMinStellenanzahl2() {
+        return minStellenanzahl2;
+    }
+
+    public void setMinStellenanzahl2(int minStellenanzahl2) {
+        this.minStellenanzahl2 = minStellenanzahl2;
+    }
+
+    public int getMaxStellenanzahl1() {
+        return maxStellenanzahl1;
+    }
+
+    public void setMaxStellenanzahl1(int maxStellenanzahl1) {
+        this.maxStellenanzahl1 = maxStellenanzahl1;
+    }
+
+    public int getMaxStellenanzahl2() {
+        return maxStellenanzahl2;
+    }
+
+    public void setMaxStellenanzahl2(int maxStellenanzahl2) {
+        this.maxStellenanzahl2 = maxStellenanzahl2;
+    }
+
+    public boolean isPlusErlaubt() {
+        return plusErlaubt;
+    }
+
+    public void setPlusErlaubt(boolean plusErlaubt) {
+        this.plusErlaubt = plusErlaubt;
+    }
+
+    public boolean isMinusErlaubt() {
+        return minusErlaubt;
+    }
+
+    public void setMinusErlaubt(boolean minusErlaubt) {
+        this.minusErlaubt = minusErlaubt;
+    }
+
+    public boolean isMalErlaubt() {
+        return malErlaubt;
+    }
+
+    public void setMalErlaubt(boolean malErlaubt) {
+        this.malErlaubt = malErlaubt;
+    }
+
+    public boolean isGeteiltErlaubt() {
+        return geteiltErlaubt;
+    }
+
+    public void setGeteiltErlaubt(boolean geteiltErlaubt) {
+        this.geteiltErlaubt = geteiltErlaubt;
+    }
+
+    public int drei () {
+        return 3;
+    }
+
+
 }
--- src/test/java/de/dikodam/libs/kopfrechentrainer/KopfrechenTrainer.java
+++ src/test/java/de/dikodam/libs/kopfrechentrainer/KopfrechenTrainer.java
@@ -1,5 +1,23 @@
 package de.dikodam.libs.kopfrechentrainer;
 
+import org.junit.Test;
+
 public class KopfrechenTrainer {
+
+    //@Tested
+    //private KopfrechenTrainer tested;
+
+    @Test
+    public void defaultConstructor(){
+        //int minStellenanzahl1 = Deencapsulation.getField(tested, "minStellenanzahl1");
+        //int minStellenanzahl2 = Deencapsulation.getField(tested, "minStellenanzahl2");
+        //int maxStellenanzahl1 = Deencapsulation.getField(tested, "maxStellenanzahl1");
+        //int maxStellenanzahl2 = Deencapsulation.getField(tested, "maxStellenanzahl2");
+        //assertThat(tested.get,is(1));
+        //assertThat(minStellenanzahl2,is(1));
+        //assertThat(maxStellenanzahl1,is(2));
+        //assertThat(maxStellenanzahl2,is(2));
+        KopfrechenTrainer tested = new KopfrechenTrainer();
+    }
 
 }
