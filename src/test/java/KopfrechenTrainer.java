4bf11e89d5ffd7576766f8088152551e8e38fd6e
adiko <dikodam@googlemail.com>
2017-07-14 20:34:31 +0200

    gitignore

--- src/test/java/KopfrechenTrainer.java
+++
@@ -1,6 +0,0 @@
-public class KopfrechenTrainer {
-    public static void main(String[] args) {
-
-    }
-
-}
---
+++ src/test/java/de/dikodam/libs/kopfrechentrainer/KopfrechenTrainer.java
@@ -0,0 +1,5 @@
+package de.dikodam.libs.kopfrechentrainer;
+
+public class KopfrechenTrainer {
+
+}
